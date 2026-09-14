10ff5b6694825cd4dcaaa43ac3b75ddfad2fc53d
ayoubhanine <ayoubhanine2000@gmail.com>
2026-09-14 16:05:54 +0100

    add appointement reducer on the store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/auth/authSlice"
+import authReducer from "../features/auth/authSlice";
+import appointmentReducer from "../features/appointments/appointmentSlice";
 export const store=configureStore({
     reducer:{
-        auth:authReducer
+        auth:authReducer,
+        appointments: appointmentReducer,
     },
 })
