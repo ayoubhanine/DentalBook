34bb6401530b1b482059be279e58ade07fa7c837
ayoubhanine <ayoubhanine2000@gmail.com>
2026-09-14 14:40:35 +0100

    feat: build admin dashboard UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
+import AdminLayout from "./layouts/AdminLayout";
+import Dashboard from "./pages/admin/Dashboard";
 function App() {
   
   return (
@@ -7,6 +9,9 @@ function App() {
    <Routes >
     <Route path="/" element={<h1 className="text-amber-300 font-bold underline">DetalBook</h1>}/>
     <Route path="/login" element={<Login/>}/>
+     <Route path="/admin" element={<AdminLayout />}>
+        <Route path="dashboard" element={<Dashboard />} />
+      </Route>
    </Routes>
     </>
   )
